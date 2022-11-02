599c8c878924e75b7fddb95c70233e50969b811a
Jeff Bellucci <jeffmbellucci@gmail.com>
2022-11-02 15:52:32 -0700

    add mock data for after hours development, update application layout, add assets

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,11 +1,15 @@
 class StationsController < ApplicationController
+  include StationsDataHelper
+  
   def index
     @stations = Station.all
   end
 
   def show
-    pp request.params
+    #pp request.params
     @station = Station.find_by_abbr(params[:id])
+    #@data = get_station_data(@station.abbr)
+    @data = mock_data
     #empty divs for appending?
     #pass time format as param?
     # lazy loading with spinner?

e20f7395f3ccd38ea5f37907e7d525a4036659be
Jeff Bellucci <jeffmbellucci@gmail.com>
2022-11-05 15:45:59 -0700

    clean up stationsController

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -8,20 +8,24 @@ class StationsController < ApplicationController
   def show
     @station = Station.find_by_abbr(params[:id])
     
-    if @station.nil? || params[:time_format] != "minutes" && params[:time_format] != "clock" 
-      render404
+    unless @station
+      render404  
     else
       @data = get_station_data(@station.abbr)
       @time_format = params[:time_format]
-      @toggle_format = params[:time_format] == "minutes" ? "clock" : "minutes"
+      @toggle_format = flip_format(params[:time_format])
       render layout: false
     end
-    # cancel PS and AI trial
     # lazy loading with spinner?
-    # Hash.from_xml?
   end
 
   def remove
     @station = Station.find_by_abbr(params[:id])
+  end
+
+  private
+
+  def flip_format(time_format)
+    time_format == "minutes" ? "clock" : "minutes"
   end
 end

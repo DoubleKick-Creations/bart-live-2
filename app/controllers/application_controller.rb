91d7dfa89e1524e08c1aba5839d682f1b9090e94
Jeff Bellucci <jeffmbellucci@gmail.com>
2022-11-05 17:28:54 -0700

    ALL STATIONS PLACED

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   
   def render404
-    respond_to do |format|
-      format.html { render file: "#{Rails.root}/public/404.html", layout: false,  status: :not_found }
-      format.xml  { head :not_found }
-      format.any  { head :not_found }
-    end
+    raise ActionController::RoutingError.new('Not Found')
   end
 end

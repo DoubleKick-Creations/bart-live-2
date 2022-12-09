0d0978ef059bdd6021b59ab69f274d25eadd28eb
Jeff Bellucci <jeffmbellucci@gmail.com>
2022-12-09 14:09:36 -0800

    hidden spinner, tooltip, reveal controller working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,2 +1,4 @@
 // Import and register all your controllers from the importmap under controllers/*
 import { application } from "./application"
+import Reveal from 'stimulus-reveal-controller'
+application.register('reveal', Reveal)

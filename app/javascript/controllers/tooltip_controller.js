70c9570f497fb402f92ec04f9d7b5027ac81d2a1
Jeff Bellucci <jeffmbellucci@gmail.com>
2022-12-19 02:19:10 -0800

    faster hiding

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -14,10 +14,17 @@ export default class extends Controller {
       if (openTooltips.length >= 1) {
         let abbr = openTooltips[0].dataset.abbr;
         let closeLink = document.getElementById(`close_${abbr}`);
+        openTooltips[0].classList.add('hidden')
         closeLink.click();
       }
       tooltip.classList.remove('hidden');
       tooltip.classList.add('openTooltip');
+    })
+  }
+
+  hide() {
+    this.tooltipTargets.forEach(tooltip => {
+      tooltip.classList.add('hidden');
     })
   }
 }
